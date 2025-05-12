24d0e8e38966e38db1a16f6c84a81d5cf98c7e07
Dhrumil <dhrumil.panchal@simformsolutions.com>
2025-05-12 16:27:19 +0530

    chore: initial template

--- src/app/components/note-list/note-list.component.ts
+++ src/app/components/note-list/note-list.component.ts
@@ -1,11 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-note-list',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './note-list.component.html',
-  styleUrl: './note-list.component.css'
+  styleUrl: './note-list.component.css',
+  standalone: true,
 })
 export class NoteListComponent {
-
+  notes = [
+    { title: 'Shopping List', description: 'Milk, Bread, Eggs' },
+    { title: 'Project Ideas', description: 'Build a personal CRM app' },
+    { title: 'Angular Tips', description: 'Use trackBy for ngFor performance' },
+  ];
 }
--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { SearchBarComponent } from '../../shared/search-bar/search-bar.component';
+import { NoteListComponent } from '../../components/note-list/note-list.component';
+import { NoteCardComponent } from '../../components/note-card/note-card.component';
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [SearchBarComponent, NoteListComponent, NoteCardComponent],
   templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.css'
+  styleUrl: './home-page.component.css',
 })
-export class HomePageComponent {
-
-}
+export class HomePageComponent {}
